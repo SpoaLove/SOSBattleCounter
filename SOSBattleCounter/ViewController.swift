82442bda2e4a56dc20226cdfc74931f22b58b7c2
Spoa <justinpu02@yahoo.com>
2018-02-19 20:24:19 +0000

    Initial Commit

---
+++ SOSBattleCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOSBattleCounter
+//
+//  Created by Tengoku no Spoa on 2018/2/19.
+//  Copyright © 2018年 Tengoku no Spoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
